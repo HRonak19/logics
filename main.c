cb43affc2f84fcee8bf6c852a0c68d0ea1e7d341
RoankCODER19 <hedambharonak@gmail.com>
2025-03-12 20:29:58 -0400

    new file

---
+++ main.c
@@ -0,0 +1,366 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Arithmetic_operators() // this function is use for Arithmetic operators select and return the selection 
+{
+    int select_Arithmetic_operators;
+    printf("\n| 1 Addition \n| 2 Subtraction \n| 3 Multiplication \n| 4 Division  \n| 5 ALL IN ONE \n|--> ");
+        scanf("%d",&select_Arithmetic_operators);
+    
+    return select_Arithmetic_operators;
+}
+
+int Number_system()
+{
+    int select_Number_system;
+
+    printf("\n| 1 Sum of 1 to user Numbers \n| 2 chack Palindrome Number \n| 3 find Factorial Number \n| 4 Armstrong Number \n| 5 Fibonacci Number \n| 6 Prime Number \n|--> ");
+    scanf("%d",&select_Number_system);
+
+    return select_Number_system;
+}
+
+    void Addition();
+    void Subtraction();
+    void Multiplication();
+    void Division();
+    void all();
+    
+
+    void sum();
+    void palindrome();
+    void factorial();
+    void armstrong();
+    void fibonacci();
+    void prime();
+
+int exit(int);
+
+void main()
+{
+    int select;
+        int select_Arithmetic,select_Number;
+
+        system("cls");
+
+            printf("\n\t\t\t\t\t- - -  A L L   L O G I C A L   O P E R A T I O N  - - - \n");
+            printf("\t\t\t\t_______________________________________________________________________");
+            printf("\n| 1 Arithmetic operators \n| 2 Number system \n| 0 Exit \n|--> ");
+            scanf("%d",&select);
+
+        system("cls");
+
+        switch (select)
+        {
+        
+            case 1:
+                    select_Arithmetic=Arithmetic_operators();
+                    system("cls");
+
+                        switch(select_Arithmetic)
+                        {
+                            
+                            case 1:
+                                Addition();
+                                    getch();
+                                main();
+                            break;
+
+                            case 2:
+                                Subtraction();
+                                    getch();
+                                main();
+                            break;
+
+                            case 3:
+                                Multiplication();
+                                    getch();
+                                main();
+                            break;
+
+                            case 4:
+                                Division();
+                                    getch();
+                                main();
+                            break;
+
+                            case 5:
+                                all();
+                                    getch();
+                                main();
+                            break;
+
+
+                        default:
+                            break;
+                        }
+            break;
+        
+            case 2:
+                    select_Number=Number_system();
+                    system("cls");
+                        switch (select_Number)
+                        {
+                            case 1:
+                                sum();
+                                    getch();
+                                main();
+                            break;
+
+                            case 2:
+                                palindrome();
+                                    getch();
+                                main();
+                            break;
+                            
+                            case 3:
+                                factorial();
+                                    getch();
+                                main();
+                            break;
+
+                            case 4:
+                                armstrong();
+                                    getch();
+                                main();
+                            break;
+
+                            case 5:
+                                fibonacci();
+                                    getch();
+                                main();
+                            break;
+
+                            case 6:
+                                prime();
+                                    getch();
+                                main();
+                            break;
+
+                        default:
+                            break;
+                        }
+            break;
+            
+            case 0:
+                exit(0);
+            break;
+
+        default:
+                printf("Please select correct number ! ");
+                getch();
+                main();
+            break;
+        }
+
+getch();
+}
+
+void Addition()
+{
+    int First,Second;
+
+        printf("| Enter First Digite : ");
+            scanf("%d",&First);
+        printf("| Enter Second Digite : ");
+            scanf("%d",&Second);
+
+    printf("|--> Here is the Addition of  %d + %d = %d",First,Second,First+Second);
+
+}
+void Subtraction()
+{
+    int First,Second;
+
+        printf("| Enter First Digite : ");
+            scanf("%d",&First);
+        printf("| Enter Second Digite : ");
+            scanf("%d",&Second);
+
+    printf("|--> Here is the  of Subtraction %d - %d = %d",First,Second,First-Second);
+
+}
+void Multiplication()
+{
+    int First,Second;
+
+        printf("| Enter First Digite : ");
+            scanf("%d",&First);
+        printf("| Enter Second Digite : ");
+            scanf("%d",&Second);
+
+    printf("|--> Here is the  of Multiplication %d * %d = %d",First,Second,First*Second);
+
+}
+void Division()
+{
+    int First,Second;
+
+        printf("| Enter First Digite : ");
+            scanf("%d",&First);
+        printf("| Enter Second Digite : ");
+            scanf("%d",&Second);
+
+    printf("|--> Here is the  of Division %d / %d = %d",First,Second,First/Second);
+
+}
+void all()
+{
+    int a,b;
+        printf("Enter First number : ");
+        scanf("%d",&a);
+        printf("Enter Second number :");
+        scanf("%d",&b);
+
+        printf("Sum   : %d \nSub   : %d \nMulti : %d \nDivi  : %d \nMode  : %d",a+b,a-b,a*b,a/b,a%b);
+}
+
+void sum()
+{
+    int user,total=0;
+
+        printf("| Enter Your number :");
+        scanf("%d",&user);
+
+            for(int i=1; i<=user; i++){
+                
+                total = total + i;
+                printf("%d",i);
+
+                    if(i!=user){
+                        printf(" + ");
+                    }
+                    else
+                        printf(" = ");
+            }
+            printf("%d ",total);
+}
+void palindrome()
+{
+    int user,rast,revers=0,tamp;
+
+        printf("| Enter any number to chack it palindrome or not : ");
+        scanf("%d",&user);
+
+        tamp = user;
+            while (user>0)
+            {
+                rast = user % 10;
+                revers = revers * 10 + rast;
+                user = user / 10;
+            }
+            if(tamp==revers){
+
+                printf("|--> %d is palindrome number !",revers);
+            }
+            else
+                printf("|--> %d is not a palindrome number !",revers);
+            
+}
+void factorial()
+{
+    int user,total=1;
+
+        printf("| Enter any number : ");
+        scanf("%d",&user);
+
+
+            for(int i=1; i<=user; i++)
+            {
+                total = total * i;
+                printf("%d",i);
+
+                if(i!=user)
+                {
+                    printf(" * ");
+                }
+                else
+                    printf(" = ");
+            
+            }
+            printf("%d ",total);
+}
+void armstrong()
+{
+    int user,r,revers=0,tamp;
+
+        printf("Enter any number to chack armstrong or not : ");
+        scanf("%d",&user);
+
+           tamp=user;
+
+            while (user>0)
+            {
+                r = user % 10;
+                revers = (r*r*r) +revers;
+                user = user / 10;
+
+            }
+            printf("%d ",revers);
+
+                if(tamp==revers){
+                    printf("is armstrong number !");
+                }
+                else
+                    printf("is not armstrong number !");
+            
+
+}
+void fibonacci()
+
+{
+    int n,a=0,b=1;
+
+        printf("Enter any number : ");
+        scanf("%d",&n);
+
+            printf("Fibonacci => %d %d ",a,b);
+    int tamp;
+
+            for(int i=1; i<=n; i++)
+            {
+                tamp = a + b;
+                    printf("%d ",tamp);
+                a=b;
+                b=tamp;
+            }
+}
+void prime()
+{
+    int n,c=0;
+
+        printf("Enter any number :");
+        scanf("%d",&n);
+        system("cls");
+
+        for(int i=1; i<=n; i++)
+        {
+            if(n % i == 0)
+            {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+            printf("%d is prime number !\n \n--> ",n);
+        }
+
+    int isprime;
+        for(int i=2; i<=n; i++)
+        {
+            isprime=1;
+
+            for(int j=2; j*j<=i; j++)
+            {
+                if(i % j == 0)
+                {
+                    isprime=0;
+                }
+            }
+            if(isprime)
+            {
+                printf("%d ",i);
+            }
+    
+    } 
+}
